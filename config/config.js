61d8ebbc31d6ef2df2789b89e9eb20edf1840f9b
Alexandre Lamberty <alexandrelamberty@gmail.com>
2023-06-03 15:57:15 +0200

    fix wrong settings

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: "root",
+    username: "minerva",
     password: "minerva",
     database: "minerva",
-    host: "172.20.0.2",
+    host: "localhost",
     port: 3306,
     dialect: "mysql",
     dialectOptions: {
