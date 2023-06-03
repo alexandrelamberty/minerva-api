5d15b262ba20ad687070f725afc82a042f808b11
Alexandre Lamberty <alexandrelamberty@gmail.com>
2023-06-03 15:59:08 +0200

    add error handling

--- src/services/openai.service.js
+++ src/services/openai.service.js
@@ -48,11 +48,28 @@ const JSON_TEMPLATE = {
   ],
 };
 
+const logError = (error) => {
+  if (error.response) {
+    console.log(error.response.status);
+    console.log(error.response.data);
+  } else {
+    console.log(error.message);
+  }
+};
+
+/**
+ * Service that use the OpenAI API
+ * https://platform.openai.com/docs/introduction
+ */
 const openaiService = {
-  /** */
+  /**
+   * Use the Completion API to suggest a category name.
+   * @returns
+   */
   suggestCategoryName: async () => {
     const prompt =
       "Could you give me a category name for a training in I.T. Respond with only the category name.";
+
     try {
       const response = await openai.createCompletion({
         model: "text-davinci-003",
@@ -69,65 +86,117 @@ const openaiService = {
         suggestion: text,
       };
     } catch (error) {
-      console.log("Error", error);
+      logError(error);
     }
   },
 
+  /**
+   * Use the Completion API to suggest a description for category name .
+   * @returns
+   */
   describeCategory: async (terms) => {
     const prompt =
       "Can you describe this information and technology category for trainings : ";
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt + terms,
-      temperature: 1,
-      max_tokens: 256,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-    const text = response.data.choices[0].text;
-    console.log(response.data);
-    return {
-      description: text,
-    };
+
+    try {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt + terms,
+        temperature: 1,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+
+      console.log(response.data);
+      const text = response.data.choices[0].text;
+      return {
+        description: text,
+      };
+    } catch (error) {
+      logError(error);
+    }
   },
 
+  /**
+   * Use the Completion API to suggest a training name.
+   * @returns
+   */
   suggestTrainingName: async () => {
     const prompt =
       "Could you suggest me a training name for a training in I.T. Respond with only the category name.";
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-      temperature: 1,
-      max_tokens: 256,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-    const text = response.data.choices[0].text;
-    console.log(response.data);
-    return {
-      description: text,
-    };
+
+    try {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 1,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+      console.log(response.data);
+      const text = response.data.choices[0].text;
+      return {
+        description: text,
+      };
+    } catch (error) {
+      logError(error);
+    }
   },
 
+  /**
+   * Use the Completion API to suggest a description for training name .
+   * @returns
+   */
   describeTraining: async (terms) => {
     const prompt =
       "Can you describe this information and technology category for trainings : ";
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt + terms,
-      temperature: 1,
-      max_tokens: 256,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-    const text = response.data.choices[0].text;
-    console.log(response.data);
-    return {
-      description: text,
-    };
+    try {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt + terms,
+        temperature: 1,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+
+      const text = response.data.choices[0].text;
+      console.log(response.data);
+      return {
+        description: text,
+      };
+    } catch (error) {
+      logError(error);
+    }
+  },
+
+  /**
+   * Use the Images API to suggest a description for training name .
+   * @returns
+   */
+  thumbnail: async (terms) => {
+    const prompt = "Image about programming";
+
+    console.log(prompt);
+    try {
+      const response = await openai.createImage({
+        prompt: prompt,
+        n: 2,
+        size: "512x512",
+      });
+      const images = response.data;
+      console.log(response.data);
+      return {
+        images: images,
+      };
+    } catch (error) {
+      logError(error);
+    }
   },
 };
 
